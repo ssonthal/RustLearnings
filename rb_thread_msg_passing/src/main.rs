1787951796cd97d5b5594bd895a955b6f6a2e117
Shubham Sonthalia <shubham18366@iiitd.ac.in>
2024-10-17 11:35:20 +0530

    correcting logic

--- rb_thread_msg_passing/src/main.rs
+++ rb_thread_msg_passing/src/main.rs
@@ -1,9 +1,32 @@
 use std::{sync::mpsc, thread::spawn};
 
+pub fn calculate() -> i64{
+    let mut ans:i64 = 0; 
+    let rec = 
+    {
+        let (tx, rx) = mpsc::channel();
+        for i in (0..10).step_by(2) {
+            let tx1 = tx.clone();
+            spawn(move || {
+                let mut sum:i64 = 0; 
+                for j in 10000000*i + 1..10000000*(i + 2) {
+                    sum += j;
+                }
+                tx1.send(sum).unwrap();
+            });
+        }
+        rx 
+    };    
+    for res in rec{
+        ans += res;
+    }
+    return ans;
+}
+
 
 // mpsc : multiple producers single producer
 fn main() {
-    // sample on how to use 
+    // sample on how to use message passing.
 
     /* 
         let (tx, rx) = mpsc::channel();
@@ -18,21 +41,5 @@ fn main() {
             Err(err) => println!("Error while reading the data -> {}", err)
         }
     */
-
-    let (tx, rx) = mpsc::channel();
-    for i in (0..10).step_by(2) {
-        let tx1 = tx.clone();
-        spawn(move || {
-            let mut sum:i64 = 0; 
-            for j in 10000000*i + 1..10000000*(i + 2) {
-                sum += j;
-            }
-            tx1.send(sum).unwrap();
-        });
-    }
-    let mut ans = 0; 
-    for res in rx{
-        ans += res;
-    }
-    println!("{}", ans);
+    println!("ans: {}", calculate());
 }
